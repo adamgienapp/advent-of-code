7dcc1d4f1718134abb560f5145dbb1bc5cfc6c40
Adam <gienappa@gmail.com>
2020-12-14 22:53:00 -0800

    added #15

---
+++ 2020/15-rambunctiousRecitation.js
@@ -0,0 +1,43 @@
+let input = [20,9,11,0,1,2];
+
+// Solution
+let findNthNumber = (starters, n) => {
+  const memory = {};
+
+  for (let i = 0; i < starters.length; i++) {
+    memory[starters[i]] = i;
+  }
+
+  let turn = starters.length;
+  let next = 0;
+  while (turn < n - 1) {
+    if (memory[next] !== undefined) {
+      let temp = next;
+      next = turn - memory[next];
+      memory[temp] = turn;
+    } else {
+      memory[next] = turn;
+      next = 0;
+    }
+    turn++;
+  }
+
+  return next;
+}
+
+// Part 1
+let t1 = [1,3,2]; // 1
+let t2 = [2,1,3]; // 10
+let t3 = [1,2,3]; // 27
+let t4 = [2,3,1]; // 78
+let t5 = [3,2,1]; // 438
+let t6 = [3,1,2]; // 1836
+
+let part1 = findNthNumber(input, 2020);
+console.log(part1);
+
+// Part 2
+let t7 = [0,3,6]; // 175594
+
+let part2 = findNthNumber(input, 30000000);
+console.log(part2);
